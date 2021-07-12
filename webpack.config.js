7e426fc3f42d79be3dc496b8c840bc415514d0d9
Adam Goodnight <adamgoodnight@Adams-MacBook-Pro.local>
2021-07-12 11:04:02 -0400

    React 16 => React 17 and Webpack 4 => Webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,108 +1,115 @@
-const Path = require('path'),
-  Webpack = require('webpack'),
-  CopyWebpackPlugin = require('copy-webpack-plugin'),
-  NodeExternals = require('webpack-node-externals'),
+const Path = require("path"),
+  Webpack = require("webpack"),
+  CopyWebpackPlugin = require("copy-webpack-plugin");
 
-  configurations = [{
-    name:'buildreact',
+configurations = [
+  {
+    name: "buildreact",
     resolve: {
-      extensions: ['*', '.js', '.jsx', '.json']
+      extensions: ["*", ".js", ".jsx", ".json"],
     },
-    target: 'web',
-    devtool: 'source-map',
-    mode: 'development',
-    entry: ['./src/index.js'],
+    target: "web",
+    devtool: "source-map",
+    mode: "development",
+    entry: ["./src/index.js"],
     output: {
-      path: Path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js',
-      publicPath: '/'
+      path: Path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+      publicPath: "/",
     },
     module: {
-      rules: [{
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.json($|\?)/,
-        use: ['json-loader'],
-        exclude: /node_modules/,
-        type: 'javascript/auto'
-      },
-      {
-        test: /.(jpg|jpeg|png|svg)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader:'file-loader',
-            options:{
-              outputPath:Path.resolve(__dirname, 'assets')
-            }
-          }
-        ]
-      }]
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ["babel-loader"],
+        },
+        {
+          test: /\.json($|\?)/,
+          use: ["json-loader"],
+          exclude: /node_modules/,
+          type: "javascript/auto",
+        },
+        {
+          test: /.(jpg|jpeg|png|svg)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                outputPath: Path.resolve(__dirname, "assets"),
+              },
+            },
+          ],
+        },
+      ],
     },
     plugins: [
       new Webpack.HotModuleReplacementPlugin(),
-      new CopyWebpackPlugin([
-        {
-          from: Path.resolve(__dirname, 'src/index.html'),
-          to: Path.resolve(__dirname, 'dist')
-        },
-        {
-          from: Path.resolve(__dirname, 'src/assets'),
-          to: Path.resolve(__dirname, 'dist/assets')
-        }
-      ])
+      new Webpack.ProvidePlugin({
+        process: "process/browser",
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: Path.resolve(__dirname, "src/index.html"),
+            to: Path.resolve(__dirname, "dist"),
+          },
+          {
+            from: Path.resolve(__dirname, "src/assets"),
+            to: Path.resolve(__dirname, "dist/assets"),
+          },
+        ],
+      }),
     ],
     devServer: {
-      contentBase: './dist',
-      index:'index.html',
-      hot: true
-    }
+      contentBase: "./dist",
+      index: "index.html",
+      hot: true,
+    },
   },
   {
-    name:'compilesass',
+    name: "compilesass",
     resolve: {
-      extensions: ['*', '.js', '.jsx', '.json']
+      extensions: ["*", ".js", ".jsx", ".json"],
     },
-    target: 'web',
-    devtool: 'source-map',
-    mode: 'development',
-    entry: ['./src/assets/sass/style.scss'],
+    target: "web",
+    devtool: "source-map",
+    mode: "development",
+    entry: ["./src/assets/sass/style.scss"],
     output: {
-      path: Path.resolve(__dirname, 'dist'),
-      publicPath: '/'
+      path: Path.resolve(__dirname, "dist"),
+      publicPath: "/",
     },
-    module:{
-      rules:[
+    module: {
+      rules: [
         {
           test: /\.scss$/,
           exclude: /node_modules/,
           use: [
             {
-              loader: 'file-loader',
+              loader: "file-loader",
               options: {
-              name: 'assets/[name].css',
-            }
+                name: "assets/[name].css",
+              },
             },
             {
-              loader: 'extract-loader'
+              loader: "extract-loader",
             },
             {
-              loader: 'css-loader?-url'
+              loader: "css-loader?-url",
             },
             {
-              loader: 'postcss-loader'
+              loader: "postcss-loader",
             },
             {
-              loader: 'sass-loader'
-            }
-          ]
-        }
-      ]
-    }
-  }
-]
+              loader: "sass-loader",
+            },
+          ],
+        },
+      ],
+    },
+  },
+];
 
-module.exports = configurations
+module.exports = configurations;
